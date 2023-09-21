82f1067cebea07f81f6ebb9ddf29961cf1d6e6e9
butchi <butchiyu+com-github@gmail.com>
2023-09-21 17:37:03 +0900

    基本実装

---
+++ index.js
@@ -0,0 +1,25 @@
+require("dotenv").config()
+
+const OpenAI = require("openai")
+
+let apiKey = process.env.OPENAI_API_KEY
+
+const inputTxt = process.argv[2]
+
+const openai = new OpenAI({
+    apiKey
+})
+
+const response = openai.chat.completions.create({
+    messages: [{
+        role: "user",
+        content: inputTxt,
+    }],
+    model: "gpt-4",
+})
+
+response.then(json => {
+    const outputTxt = json?.choices?.[0].message?.content
+
+    console.log(outputTxt)
+})
