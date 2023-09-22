78c619ec592412283be630b74a2360c9b167176a
butchi <butchiyu+com-github@gmail.com>
2023-09-22 15:02:27 +0900

    空コマンド対処

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 #!/usr/bin/env node
-const inputTxt = process.argv.slice(2).join(" ")
+const inputTxt = process.argv[2] ? process.argv.slice(2).join(" ") : null
 
 const headers = new Headers({ "Content-Type": "application/json" })
 
-const response = fetch("https://dvl677f2xu3n44xqpcolv64iwe0jbmyd.lambda-url.eu-north-1.on.aws/", {
-    method: "POST",
-    body: JSON.stringify({
-        text: inputTxt,
-    }),
-    headers,
-})
-
-response.then(res => {
-    res.json().then(json => {
-        console.log(json)
+if (inputTxt == null) {
+    console.log("使用例: ")
+    console.log("chottokiku 黄金比の近似値は？")
+} else {
+    const response = fetch("https://dvl677f2xu3n44xqpcolv64iwe0jbmyd.lambda-url.eu-north-1.on.aws/", {
+        method: "POST",
+        body: JSON.stringify({
+            text: inputTxt,
+        }),
+        headers,
     })
-})
+    
+    response.then(res => {
+        res.json().then(json => {
+            console.log(json)
+        })
+    })
+}
