caff7f7684d8f76b3866aa788ef8f6fb516aeef5
butchi <butchiyu+com-github@gmail.com>
2023-09-22 16:33:41 +0900

    応答の仕様変更

--- lambda/index.mjs
+++ lambda/index.mjs
@@ -16,6 +16,9 @@ export const handler = async evt => {
         resJson = await openai.chat.completions.create({
             messages: [{
                 role: "user",
+                content: "返答は15文字以内、長くて85文字までの短い文章でお願いします。",
+            }, {
+                role: "user",
                 content: inputTxt,
             }],
             model: "gpt-4",
@@ -23,7 +26,7 @@ export const handler = async evt => {
     } catch (err) {
         const response = {
             statusCode: 500,
-            body: err,
+            body: JSON.stringify(err),
         }
         return response
     }
