6f4a9f9f583f6340b17546a7a603c33d33dd8d04
butchi <butchiyu+com-github@gmail.com>
2023-09-22 13:47:17 +0900

    フロントエンド再実装

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
 #!/usr/bin/env node
-const OpenAI = require("openai")
+const inputTxt = process.argv.slice(2).join(" ")
 
-const apiKey = "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"
+const headers = new Headers({ "Content-Type": "application/json" })
 
-const inputTxt = process.argv[2]
-
-const openai = new OpenAI({
-    apiKey
+const response = fetch("https://dvl677f2xu3n44xqpcolv64iwe0jbmyd.lambda-url.eu-north-1.on.aws/", {
+    method: "POST",
+    body: JSON.stringify({
+        text: inputTxt,
+    }),
+    headers,
 })
 
-const response = openai.chat.completions.create({
-    messages: [{
-        role: "user",
-        content: inputTxt,
-    }],
-    model: "gpt-4",
-})
-
-response.then(json => {
-    const outputTxt = json?.choices?.[0].message?.content
-
-    console.log(outputTxt)
-
-    // console.info("※ 本プログラムはサービスの永続性を保証するものではありません")
+response.then(res => {
+    res.json().then(json => {
+        console.log(json)
+    })
 })
