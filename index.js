813c43d20fe753cbac48da909748011a69351b81
butchi <butchiyu+com-github@gmail.com>
2023-09-22 16:33:24 +0900

    エラーハンドリング修正

--- index.js
+++ index.js
@@ -7,17 +7,23 @@ if (inputTxt == null) {
     console.log("使用例: ")
     console.log("chottokiku 黄金比の近似値は？")
 } else {
-    const response = fetch("https://dvl677f2xu3n44xqpcolv64iwe0jbmyd.lambda-url.eu-north-1.on.aws/", {
-        method: "POST",
-        body: JSON.stringify({
-            text: inputTxt,
-        }),
-        headers,
-    })
-    
-    response.then(res => {
-        res.json().then(json => {
-            console.log(json)
+    try {
+        const response = fetch("https://dvl677f2xu3n44xqpcolv64iwe0jbmyd.lambda-url.eu-north-1.on.aws/", {
+            method: "POST",
+            body: JSON.stringify({
+                text: inputTxt,
+            }),
+            headers,
         })
-    })
+        
+        response.then(res => {
+            res.text().then(txtJson => {
+                const txt = txtJson === "Internal Server Error" ? "サーバーエラー" : JSON.parse(txtJson)
+
+                console.log(txt)
+            })
+        })
+    } catch (err) {
+        console.error("実行エラー")
+    }
 }
